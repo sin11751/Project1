18c472a5fcea01a4d1d16ec4a03ad7659a2b5420
kohliash <kohliash@sheridancollege.ca>
2022-03-05 11:35:05 -0500

    Done!

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -8,25 +8,25 @@ public class Project1 {
 
     
     public static void main(String[] args) {
-     System.out.println("                  Guess game");  
+     System.out.println("                  Guess game");   // name of the game
      System.out.println("-----------------------------------------------");
      System.out.println();
-    System.out.println("// Please enter a positive number below ten  //");
+    System.out.println("// Please enter a positive number below ten  //"); // user has to enter a postive number between 0 and 9
     int turn=1;
     int confirm=0;
        
-       while (confirm==0){
+       while (confirm==0){  
                System.out.print("Enter number:-");
     Scanner num=new Scanner (System.in);
-    int uvalue = num.nextInt();
-    int cvalue = (int)(Math.random()*11);
+    int uvalue = num.nextInt(); // number entered by user is saved in uvalue
+    int cvalue = (int)(Math.random()*11); // random number choosed by computer using Math.random
     
-        while(uvalue!=cvalue)
+        while(uvalue!=cvalue) // if the uvalue is not equal to cvalue user is asked to enter the number until it is equal
         {  turn++;
-            if (uvalue>cvalue)
+            if (uvalue>cvalue) //condition that would tell uvalue is greater than cvalue is it's smaller else would work
             {System.out.println(uvalue +" is greater than than the Computer choosed number");
             System.out.println();  
-            System.out.println("Enter an another number"); 
+            System.out.println("Enter an another number");// user is asked to enter the number again and if user enterd same number as computer game will be over, if not user will again be asked to enter a number again
               System.out.print("-->");
               uvalue = num.nextInt();}
         
@@ -39,12 +39,12 @@ public class Project1 {
          
           }
       
-       System.out.println("Player choose the right number after "+ turn + " turn");
+       System.out.println("Player choose the right number after "+ turn + " turn"); // would display after how many turns user got the same number as computer
        System.out.println();
-       System.out.println("If player want to play more then enter '0' for 'YES' and '1' for 'NO'");
+       System.out.println("If player want to play more then enter '0' for 'YES' and '1' for 'NO'"); // confirmation if the user want to play again or not
                System.out.print("-->");
               confirm= num.nextInt();
-              
+              // if user will write 0 then again will start and if the user will write 1 then on screen it will show End game and would exit game
               if(confirm==0)
              {System.out.println();
                  System.out.println("-------------------  NEW GAME ----------------------------------- ");} 
@@ -59,6 +59,3 @@ public class Project1 {
             
                
     }              
-
-    
-
